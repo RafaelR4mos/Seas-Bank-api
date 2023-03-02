313ff1c32c1a954cde76a3d5bc767212ac4b3183
Rafael <rafaelramos0401@gmail.com>
2023-03-02 00:59:33 -0300

    fixed: style in signup, added: search bar in dashboar page

--- sign-up/js/script.js
+++ sign-up/js/script.js
@@ -1,19 +1,28 @@
+const cpfInput = document.getElementById("cep-input");
+
+cpfInput.addEventListener("input", () => {
+    const cpfInputValue = cpfInput.value;
+
+    if (cpfInputValue.length == 8) {
+        buscarCep(cpfInputValue);
+    }
+});
+
 function buscarCep(cep) {
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.erro) {
-          alert("CEP não encontrado.");
-        } else {
-          document.getElementById("addres").value = data.logradouro;
-          document.getElementById("bairro").value = data.bairro;
-          document.getElementById("localidade").value = data.localidade;
-          document.getElementById("uf").value = data.uf;
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        alert("Ocorreu um erro ao buscar o CEP.");
-      });
-  }
-  
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.erro) {
+                alert("CEP não encontrado.");
+            } else {
+                document.getElementById("addres").value = data.logradouro;
+                document.getElementById("bairro").value = data.bairro;
+                document.getElementById("localidade").value = data.localidade;
+                document.getElementById("uf").value = data.uf;
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("Ocorreu um erro ao buscar o CEP.");
+        });
+}
