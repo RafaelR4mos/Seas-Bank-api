9b5d6da46c9b5e84c1b0485eb716c2b6e36b94a8
Rayane da Silva Costa <rayane.costa@dbccompany.com.br>
2023-03-02 00:11:47 -0300

    API de consulta do CEP

---
+++ sign-up/js/script.js
@@ -0,0 +1,19 @@
+function buscarCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.erro) {
+          alert("CEP não encontrado.");
+        } else {
+          document.getElementById("addres").value = data.logradouro;
+          document.getElementById("bairro").value = data.bairro;
+          document.getElementById("localidade").value = data.localidade;
+          document.getElementById("uf").value = data.uf;
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        alert("Ocorreu um erro ao buscar o CEP.");
+      });
+  }
+  
