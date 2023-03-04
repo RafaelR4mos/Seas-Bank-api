ba28b6cd2a73811cacd7526acd4e84ce1cfd0b01
Rafael <rafaelramos0401@gmail.com>
2023-03-04 15:50:00 -0300

    added dynamic fetch to avaliar section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,65 +8,87 @@ const commentTitle = document.getElementById("comment-title");
 const commentText = document.getElementById("comment-text");
 const testimonialsContainer = document.getElementById("testimonials-container");
 
-var posts = [
-    {
-        name: "thassia silva",
-        email: "thassia.silva@gmail.com",
-        plan: "platinum",
-        title: "Ótimos Planos",
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-    {
-        name: "bruno costa",
-        email: "bruno.costa@gmail.com",
-        plan: "gold",
-        title: "Ótimo atendimento da equipe de suporte",
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-    {
-        name: "Felipe Ferreira",
-        email: "felipe.ferreira@gmail.com",
-        plan: "silver",
-        title: "Plano acessível e com vantagens",
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-];
+// var posts = [
+//     {
+//         name: "thassia silva",
+//         email: "thassia.silva@gmail.com",
+//         plan: "platinum",
+//         title: "Ótimos Planos",
+//         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     },
+//     {
+//         name: "bruno costa",
+//         email: "bruno.costa@gmail.com",
+//         plan: "gold",
+//         title: "Ótimo atendimento da equipe de suporte",
+//         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     },
+//     {
+//         name: "Felipe Ferreira",
+//         email: "felipe.ferreira@gmail.com",
+//         plan: "silver",
+//         title: "Plano acessível e com vantagens",
+//         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     },
+// ];
 
-window.addEventListener("DOMContentLoaded", () => {
-    setComments(
-        posts.map((post) => {
-            return post;
-        })
-    );
-});
+fetch("https://jsonplaceholder.typicode.com/posts/")
+    .then((response) => response.json())
+    .then((comment) => {
+        console.log(comment);
+        setComments(
+            comment.filter((comment) => {
+                if (comment.id <= 3) {
+                    return comment;
+                }
+            })
+        );
+    });
 
 function setComments(results) {
-    let i = 0;
-    for (comments of results) {
-        console.log(results);
+    console.log(Array.isArray(results));
+
+    if (Array.isArray(results)) {
+        let i = 0;
+        for (comments of results) {
+            const testimonialCard = document.createElement("div");
+            testimonialCard.classList.add("testimonial-card");
+            // testimonialCard.classList.add("pre-rendered-cards");
+            console.log(i);
+            console.log(results);
+            const commentData = `
+            <div class="card-header">
+                <strong>${results[i].title}</strong>
+            </div>
+    
+    
+            <div class="card-text">
+                <p>
+                    ${results[i].body}
+                </p>
+            </div>`;
+
+            testimonialCard.innerHTML = commentData;
+            testimonialsContainer.appendChild(testimonialCard);
+            i++;
+        }
+    } else {
         const testimonialCard = document.createElement("div");
         testimonialCard.classList.add("testimonial-card");
-
+        // testimonialCard.classList.add("new-card");
         const commentData = `
         <div class="card-header">
-            <strong>${results[i].name}</strong>
-            <span class="${results[i].plan}">Seas ${results[i].plan}</span>
-            <div class="card-badget ${results[i].plan}"></div>
-        </div>
-
-        <div class="card-title">
-            <span>${results[i].title}</span>
+            <strong>${results.title}</strong>
         </div>
 
         <div class="card-text">
             <p>
-                ${results[i].text}
+                ${results.body}
             </p>
         </div>`;
 
         testimonialCard.innerHTML = commentData;
-        testimonialsContainer.appendChild(testimonialCard);
-        i++;
+        testimonialsContainer.prepend(testimonialCard);
     }
 }
 
@@ -79,15 +101,31 @@ function clearList() {
 function getFormData(e) {
     e.preventDefault();
 
-    var commentData = {
-        name: commentName.value,
-        email: commentEmail.value,
-        plan: commentPlan.value,
-        title: commentTitle.value,
-        text: commentText.value,
-    };
+    const title = commentTitle.value;
+    const text = commentText.value;
 
-    posts.push(commentData);
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            title: title,
+            body: text,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            setComments(json);
+        });
+
+    // var commentData = {
+    //     name:
+    //     email: commentEmail.value,
+    //     plan: commentPlan.value,
+    //     title:
+    // };
 
     commentName.value = "";
     commentEmail.value = "";
@@ -95,12 +133,12 @@ function getFormData(e) {
     commentTitle.value = "";
     commentText.value = "";
 
-    clearList();
-    setComments(
-        posts.map((post) => {
-            return post;
-        })
-    );
+    // clearList();
+    // setComments(
+    //     posts.map((post) => {
+    //         return post;
+    //     })
+    // );
     btnClose.click();
 }
 
